0901d7834e0f358321ff4e78d32cad11fac369e9
SergGrane <94228017+SergGrane@users.noreply.github.com>
2021-11-13 17:15:06 +0200

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,35 @@
+print('Hello World!')     # task1
+
+print('This is', '\n' "the homework", '\n' 'number two')      # task 2
+
+length=int(input ('length = '))                               # task 3
+width=int(input('width = '))
+print('square = ',length*width)
+
+
+x=float(input('number1 = '))                                # task 4
+y=float(input('number2 = '))
+summ=x+y
+prod=x*y
+differ=x-y
+quotient1=x/y
+quotient2=x//y
+quotient3=x%y
+print('sum = ',summ)
+print('product = ',prod)
+print('difference = ',differ)
+print('quotient1 = ',quotient1)
+print('quotient2 = ',quotient2)
+print('quotient3 = ',quotient3)
+
+
+import math                                                 # task 5
+math.pi
+rad=float(input('Radius = '))
+diam=rad*2
+circum=2*math.pi*rad
+area=math.pi**2
+print('rad = ',rad)
+print('diam = ',diam)
+print('circum = ',circum)
+print('area = ',area)
